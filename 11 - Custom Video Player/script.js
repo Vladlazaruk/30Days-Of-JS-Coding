42221f3cf1afde45baeba88cf99a3eac33d56a23
Vladyslav <vladlazaruk@gmail.com>
2021-02-05 23:41:44 +0100

    made player

---
+++ 11 - Custom Video Player/script.js
@@ -0,0 +1,56 @@
+const player = document.querySelector('.player'),
+    video = document.querySelector('.viewer'),
+    progress = document.querySelector('.progress'),
+    progressBar = document.querySelector('.progress__filled'),
+    toggle = document.querySelector('.toggle'),
+    skipBtns = document.querySelectorAll('[data-skip]'),
+    ranges = document.querySelectorAll('.player__slider'),
+    full = document.querySelector('.player__full-screen');
+
+
+function togglePlay(){
+    if(video.paused){
+        video.play();
+        toggle.textContent = '❚ ❚';
+    } else{
+        video.pause();
+        toggle.textContent = '►';
+    }
+}
+
+function skip(){
+    video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRange(){
+    video[this.name] = this.value;
+}
+
+function handleProgress(){
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e){
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+}
+
+function fullScreen(){
+    console.log(window.offsetWidth);
+}
+
+video.addEventListener('click', togglePlay);
+video.addEventListener('timeupdate', handleProgress);
+toggle.addEventListener('click', togglePlay);
+skipBtns.forEach(btn =>{
+    btn.addEventListener('click', skip);
+});
+ranges.forEach(item =>{
+    item.addEventListener('change', handleRange);
+});
+ranges.forEach(item =>{
+    item.addEventListener('mousmove', handleRange);
+});
+progress.addEventListener('click', scrub);
+full.addEventListener('click', fullScreen);
