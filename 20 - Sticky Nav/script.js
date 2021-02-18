ef99f596aadef042ae075b12bc96986030b062ad
Vladyslav <vladlazaruk@gmail.com>
2021-02-18 12:06:22 +0100

    added fixed position in style and made script

---
+++ 20 - Sticky Nav/script.js
@@ -0,0 +1,14 @@
+const nav = document.querySelector('#main');
+    let topOfNav = nav.offsetTop;
+
+    function fixNav() {
+      if (window.scrollY >= topOfNav) {
+        document.body.style.paddingTop = nav.offsetHeight + 'px';
+        nav.classList.add('fixed-nav');
+      } else {
+        nav.classList.remove('fixed-nav');
+        document.body.style.paddingTop = 0;
+      }
+    }
+
+    window.addEventListener('scroll', fixNav);
